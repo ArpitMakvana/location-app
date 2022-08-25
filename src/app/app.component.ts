2547b3f841ad3d7800726a49b2c69e29b0a3552a
arpit <arpit.makvana@hiteshi.com>
2022-08-25 14:21:05 +0530

    MOVED TO CORDOVA vl 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
-// import { SwUpdate } from '@angular/service-worker';
-
 import { MenuController, Platform, ToastController } from '@ionic/angular';
-
-// import { StatusBar } from '@ionic/core';
-// import { SplashScreen } from '@capacitor/splash-screen';
-
 import { Storage } from '@ionic/storage-angular';
-
 import { UserData } from './providers/user-data';
 // import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationEvents, BackgroundGeolocationResponse } from '@awesome-cordova-plugins/background-geolocation/ngx';
 
@@ -50,7 +43,6 @@ export class AppComponent implements OnInit {
     private router: Router,
     private storage: Storage,
     private userData: UserData,
-    // private swUpdate: SwUpdate,
     private toastCtrl: ToastController,
     // private backgroundGeolocation: BackgroundGeolocation
   ) {
@@ -61,34 +53,12 @@ export class AppComponent implements OnInit {
     await this.storage.create();
     this.checkLoginStatus();
     this.listenForLoginEvents();
-
-    // this.swUpdate.available.subscribe(async res => {
-    //   const toast = await this.toastCtrl.create({
-    //     message: 'Update available!',
-    //     position: 'bottom',
-    //     buttons: [
-    //       {
-    //         role: 'cancel',
-    //         text: 'Reload'
-    //       }
-    //     ]
-    //   });
-
-    //   await toast.present();
-
-    //   toast
-    //     .onDidDismiss()
-    //     .then(() => this.swUpdate.activateUpdate())
-    //     .then(() => window.location.reload());
-    // });
   }
 
   initializeApp() {
     this.platform.ready().then(async() => {
       
       if (this.platform.is('hybrid')) {
-        // StatusBar.hide();
-        // SplashScreen.hide();
       } 
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,13 @@
-// import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// // import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-// import { IonicModule } from '@ionic/angular';
-// import { IonicStorageModule } from '@ionic/storage-angular';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// // import { ServiceWorkerModule } from '@angular/service-worker';
-// import { environment } from '../environments/environment';
-// import { FormsModule } from '@angular/forms';
-// import { HttpConfigInterceptor } from './interceptor/httpConfig.interceptor';
-// // import { SafePipe } from './pipes/safe.pipe';
-// // import { BackgroundGeolocation } from '@awesome-cordova-plugins/background-geolocation/ngx';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpConfigInterceptor } from './interceptor/httpConfig.interceptor';
 import { IonicStorageModule } from '@ionic/storage-angular';
-// import { SafePipe } from './pipes/safe.pipe';
 
 
 @NgModule({
@@ -48,29 +30,4 @@ FormsModule,
 ],
   bootstrap: [AppComponent],
 })
-
-// @NgModule({
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule,
-//     FormsModule,
-//     IonicModule.forRoot({
-//       mode: 'ios'
-//   }),
-//     IonicStorageModule.forRoot(),
-//     // ServiceWorkerModule.register('ngsw-worker.js', {
-//     //   enabled: environment.production
-//     // })
-//   ],
-//   declarations: [AppComponent],
-//   providers: [
-//     {
-//       provide: HTTP_INTERCEPTORS,
-//       useClass: HttpConfigInterceptor,
-//       multi: true
-//     }
-//   ],
-//   bootstrap: [AppComponent]
-// })
 export class AppModule {}
